1c03c7294752d59c33751d6d9c370ab84327968c
Adarsh Goswami <63430847+adarsh-goku@users.noreply.github.com>
2020-07-22 03:24:26 +0530

    Create #1503: Last Moment Before all and fall out of a plank.java

---
+++ #1503: Last Moment Before all and fall out of a plank.java
@@ -0,0 +1,17 @@
+/*
+  Approach: It is a brain teaser as it does'nt matters whether ants meet or not. We can think about this like given position and length tell when it will fall.
+            O( N ) time and O( 1 ) space complexity.
+*/
+
+class Solution {
+    public int getLastMoment(int n, int[] left, int[] right) 
+    {
+        int ans=0;
+        
+        for(int i=0;i<left.length;i++)
+            ans= Math.max(ans, left[i]);
+        for(int i=0;i<right.length;i++)
+            ans= Math.max(ans, n-right[i]);
+        return ans;
+    }
+}
