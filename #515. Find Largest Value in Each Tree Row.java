4945d2632402f02ad38fae8fb5b1dfdf13a326d7
Adarsh Goswami <63430847+Adarsh-Goswamii@users.noreply.github.com>
2020-08-17 01:55:40 +0530

    Create #515. Find Largest Value in Each Tree Row.java

---
+++ #515. Find Largest Value in Each Tree Row.java
@@ -0,0 +1,46 @@
+// BFS
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> largestValues(TreeNode root) 
+    {
+        ArrayList<Integer> ans=new ArrayList<>();
+        Queue<TreeNode> q=new LinkedList<>();
+        if(root!=null)
+            q.add(root);
+        
+        while(!q.isEmpty())
+        {
+            int size= q.size();
+            int max= Integer.MIN_VALUE;
+            
+            for(int i=0;i<size;i++)
+            {
+                TreeNode curr= q.poll();
+                max= Math.max(max, curr.val);
+                
+                if(curr.left!=null)
+                    q.add(curr.left);
+                
+                if(curr.right!=null)
+                    q.add(curr.right);
+            }
+            ans.add(max);
+        }
+        return  ans;
+    }
+}
