44eba7e91508f81b980501f9415dc2917710393f
Adarsh Goswami <63430847+Adarsh-Goswamii@users.noreply.github.com>
2020-08-18 03:53:07 +0530

    Create #946. Validate Stack Sequences.java

---
+++ #946. Validate Stack Sequences.java
@@ -0,0 +1,25 @@
+//946. Validate Stack Sequences
+
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) 
+    {
+        Stack<Integer> stack=new Stack<>();
+        stack.push(-1);
+        int pop=0, push=0;
+        
+        while(pop!=popped.length)
+        {
+            while(push!= pushed.length && stack.peek()!= popped[pop])
+                stack.push(pushed[push++]);
+            
+            if(stack.peek()!= popped[pop])
+                return false;
+            
+            stack.pop();
+            pop++;
+        }
+        System.out.println(pop);
+        
+        return pop== popped.length;
+    }
+}
