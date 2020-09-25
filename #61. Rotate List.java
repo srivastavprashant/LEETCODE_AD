9e934580e584ea8d6a3af255dd41cb3400330334
Adarsh Goswami <63430847+Adarsh-Goswamii@users.noreply.github.com>
2020-09-25 08:43:56 +0530

    Create #61. Rotate List.java

---
+++ #61. Rotate List.java
@@ -0,0 +1,37 @@
+// 61. Rotate List
+
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) 
+    {
+        if(null== head) return null;
+        int len=0;
+        ListNode curr= head, last= null;
+        
+        while(curr!= null)
+        {
+            last = curr;
+            curr= curr.next;
+            len++;
+        }
+        
+        k=k%len;
+        if(k==0) return head;
+        k= len-k;
+        curr= head;
+        len=0;
+        
+        while(len+1!=k)
+        {
+            curr= curr.next;
+            len++;
+        }
+        
+        //System.out.println(curr.next.val);
+        ListNode newHead= curr.next;
+        curr.next= null;
+        last.next= head;
+        
+        return newHead;
+        
+    }
+}
