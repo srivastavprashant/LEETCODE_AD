be97a113d22b8be6b56f5cfcbf3ed95d6b575bb6
Adarsh Goswami <63430847+Adarsh-Goswamii@users.noreply.github.com>
2020-10-09 00:18:28 +0530

    Create #1588. Sum of All Odd Length Subarrays.java

---
+++ #1588. Sum of All Odd Length Subarrays.java
@@ -0,0 +1,19 @@
+// 1588. Sum of All Odd Length Subarrays
+
+class Solution 
+{
+    public int sumOddLengthSubarrays(int[] arr) 
+    {
+        int ans=0;
+        for(int i=1;i<arr.length;i++)
+            arr[i]+= arr[i-1];
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i;j<arr.length;j+=2)
+                ans+= arr[j]- (i!=0? arr[i-1]: 0);
+        }
+        
+        return ans;
+    }
+}
