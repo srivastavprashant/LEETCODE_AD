5f6600106bc591f87c62bc359f411a93eed033f2
Adarsh Goswami <63430847+Adarsh-Goswamii@users.noreply.github.com>
2020-12-09 17:42:48 +0530

    Update #130. Surrounded Regions.java

--- #130. Surrounded Regions.java
+++ #130. Surrounded Regions.java
@@ -60,3 +60,100 @@ class Solution
  ["X","O","X","O","X","O"],
  ["O","X","O","X","O","X"]]
 */
+
+// Approach 2: union find
+
+class Solution {
+    public void solve(char[][] board) {
+        if(board.length==0) return;
+        
+        UnionFind dsu= new UnionFind(board.length* board[0].length+1);
+        int col= board[0].length;
+        for(int i=0;i<board.length;i++)
+        {
+            for(int j=0;j<board[0].length;j++)
+            {
+                if(i==0 || i== board.length-1 || j==0 || j== board[0].length-1)
+                    dsu.union(i*col+ j, board[0].length* board.length);
+                else if(board[i][j]=='O')
+                {
+                    if(board[i-1][j]== 'O')
+                        dsu.union((i-1)*col+ j, i*col+ j);
+                    
+                    if(board[i+1][j]== 'O')
+                        dsu.union((i+1)*col+ j, i*col+ j);
+                    
+                    if(board[i][j-1]== 'O')
+                        dsu.union(i*col+ j-1, i*col+ j);
+                    
+                    if(board[i][j+1]== 'O')
+                        dsu.union(i*col+ j+1, i*col+ j);
+                }
+            }
+        }
+        
+        for(int i=0;i<board.length;i++)
+            for(int j=0;j<board[0].length;j++)
+                if(!dsu.isConnected(i*col+ j, board.length* board[0].length) && board[i][j]== 'O')
+                    board[i][j]= 'X';
+    }
+}
+
+class UnionFind
+{
+    int[] rank, par;
+    UnionFind(int size)
+    {
+        rank= new int[size];
+        par= new int[size];
+        
+        for(int i=0;i<size;i++)
+            par[i]= i;
+    }
+    
+    void union(int a, int b)
+    {
+        while(par[a]!= a)
+            a= par[a];
+        
+        while(par[b]!= b)
+            b= par[b];
+        
+        if(rank[a]> rank[b])
+            par[b]= a;
+        else if(rank[b]> rank[a])
+            par[a]= b;
+        else 
+        {
+            rank[a]++;
+            par[b]= a;
+        }
+    }
+    
+    boolean isConnected(int a, int b)
+    {
+        int tempA= a, tempB= b;
+        while(par[a]!= a)
+            a= par[a];
+        
+        while(par[b]!= b)
+            b= par[b];
+        
+        while(par[tempA]!= tempA)
+        {
+            int temp= tempA;
+            tempA= par[tempA];
+            par[temp]= a;
+        }
+        
+        while(par[tempB]!= tempB)
+        {
+            int temp= tempB;
+            tempB= par[tempB];
+            par[temp]= b;
+        }
+        
+        return a==b;
+    }
+    
+}
