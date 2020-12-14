0e1a30abe64fd5bdbff2332eb3286209d346edd8
Adarsh Goswami <63430847+Adarsh-Goswamii@users.noreply.github.com>
2020-12-15 02:54:57 +0530

    Create #856. Score of Parentheses.java

---
+++ #856. Score of Parentheses.java
@@ -0,0 +1,30 @@
+// #856. Score of Parentheses
+
+
+class Solution {
+    public int scoreOfParentheses(String S) 
+    {
+        Stack<Integer> stack= new Stack<>();
+        for(char c: S.toCharArray())
+        {
+            if(c== '(')
+                stack.push(-1);
+            else
+            {
+                int val= 0;
+                while(stack.peek()!= -1)
+                    val+= stack.pop();
+                
+                stack.pop();
+                if(val== 0) stack.push(1);
+                else stack.push(val*2);
+            }
+        }
+        
+        int ans= 0;
+        while(!stack.isEmpty())
+            ans+= stack.pop();
+        
+        return ans;
+    }
+}
