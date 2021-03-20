d545847195aad6598fed03e493d079e83655a797
Adarsh Goswami <63430847+Adarsh-Goswamii@users.noreply.github.com>
2021-03-20 11:42:34 +0530

    Create #646. Maximum Length of Pair Chain.java

---
+++ #646. Maximum Length of Pair Chain.java
@@ -0,0 +1,49 @@
+// #646. Maximum Length of Pair Chain
+
+
+// Approach 1 : Dynamic programming
+class Solution 
+{
+    HashMap<Integer, Integer> memo;
+    int call(int[][] pairs, int curr)
+    {
+        if(memo.containsKey(curr)) return memo.get(curr);
+        else
+        {
+            int index= binarySearch(pairs, curr), max= 0;
+            
+            for(int i= index;i<pairs.length;i++)
+                max= Math.max(max, 1+ call(pairs, pairs[i][1]));
+            
+            memo.put(curr, max);
+            return max;
+        }
+    }
+    
+    int binarySearch(int[][] pairs, int find)
+    {
+        int ret= pairs.length, start= 0, last= pairs.length-1;
+        while(start<= last)
+        {
+            int mid= start+ (last- start)/2;
+            
+            if(find< pairs[mid][0])
+            {
+                ret= mid;
+                last= mid-1;
+            }
+            else
+                start= mid+ 1;
+        }
+        
+        return ret;
+    }
+    
+    public int findLongestChain(int[][] pairs)
+    {
+        Arrays.sort(pairs, (a, b)-> (a[0]- b[0]));
+        memo= new HashMap<>();
+        
+        return call(pairs, Integer.MIN_VALUE);
+    }
+}
