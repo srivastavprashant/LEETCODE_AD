6dbb02780ac9a69aaaffabf31b0c651cc1252e57
Adarsh Goswami <63430847+Adarsh-Goswamii@users.noreply.github.com>
2021-03-31 02:38:03 +0530

    Create #838. Push Dominoes.java

---
+++ #838. Push Dominoes.java
@@ -0,0 +1,41 @@
+// 838. Push Dominoes
+
+
+
+class Solution {
+    public String pushDominoes(String dominoes) 
+    {
+        char[] arr= dominoes.toCharArray();
+        boolean[] visited= new boolean[arr.length];
+        Queue<Integer> q= new LinkedList<>();
+        for(int i=0;i<arr.length;i++)
+            if(arr[i]!= '.')
+            { q.add(i); visited[i]= true; }
+        
+        while(!q.isEmpty())
+        {
+            int size= q.size();
+            HashSet<Integer> set= new HashSet<>();
+            for(int ii=0;ii<size;ii++)
+            {
+                int i= q.poll();
+                if(arr[i]== '.' ) continue;
+                
+                if(arr[i]== 'L' && i!= 0)
+                { 
+                    if(set.contains(i-1)) arr[i-1]= '.';
+                    else if(!visited[i-1])
+                    { arr[i-1]= 'L'; q.add(i-1); set.add(i-1); visited[i-1]= true; }
+                }
+                else if(arr[i]== 'R' && i!= arr.length-1)
+                { 
+                    if(set.contains(i+1)) arr[i+1]= '.';
+                    else if(!visited[i+1]) 
+                    { arr[i+1]= 'R'; q.add(i+1); set.add(i+1); visited[i+1]= true; }
+                }
+            }
+        }
+        
+        return new String(arr);
+    }
+}
