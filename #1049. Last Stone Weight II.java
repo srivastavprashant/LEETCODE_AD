5dd77dbf1fd71087a6b1ecaccc2146b12f8084dd
Adarsh Goswami <63430847+Adarsh-Goswamii@users.noreply.github.com>
2021-04-14 01:59:16 +0530

    Create #1049. Last Stone Weight II.java

---
+++ #1049. Last Stone Weight II.java
@@ -0,0 +1,28 @@
+// #1049. Last Stone Weight II
+
+
+
+class Solution {
+    public int lastStoneWeightII(int[] stones) {
+        int sum= 0;
+        for(int i: stones) sum+= i;
+        
+        boolean[] dp= new boolean[sum+1];
+        dp[0]= true;
+        for(int i: stones) {
+            boolean[] temp= new boolean[sum+1];
+            for(int j=0;j<sum+1;j++) {
+                if(j< i) temp[j]= dp[j];
+                else temp[j]|= dp[j]| dp[j-i];
+            }
+            
+            dp= temp;
+        }
+        
+        int ans= sum;
+        for(int i=0;i<sum+1;i++) 
+            if(dp[i]) ans= Math.min(ans, Math.abs(i- (sum-i)));
+        
+        return ans;
+    }
+}
