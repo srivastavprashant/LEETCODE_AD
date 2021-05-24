afab74b78a8579664135af8025a26c9c8ed6ecf8
Adarsh Goswami <63430847+Adarsh-Goswamii@users.noreply.github.com>
2021-05-24 18:52:49 +0530

    Create #1869. Longer Contiguous Segments of Ones than Zeros.java

---
+++ #1869. Longer Contiguous Segments of Ones than Zeros.java
@@ -0,0 +1,20 @@
+// #1869. Longer Contiguous Segments of Ones than Zeros
+
+
+class Solution {
+    public boolean checkZeroOnes(String s) {
+        return count(s.toCharArray(), '1')>count(s.toCharArray(), '0');
+    }
+    
+    int count(char[] arr, char ch) {
+        int ret= 0, cou= 0;
+        for(char c: arr) {
+            if(c== ch) cou++;
+            else cou= 0;
+            
+            ret= Math.max(ret, cou);
+        }
+        
+        return ret;
+    }
+}
