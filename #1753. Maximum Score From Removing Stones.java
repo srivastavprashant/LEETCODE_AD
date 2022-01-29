e1c0c91459b61d4728684f95de2c78f87c36e75c
Adarsh Goswami <63430847+Adarsh-Goswamii@users.noreply.github.com>
2022-01-29 16:45:16 +0530

    Create #1753. Maximum Score From Removing Stones.java

---
+++ #1753. Maximum Score From Removing Stones.java
@@ -0,0 +1,12 @@
+
+
+class Solution {
+    public int maximumScore(int a, int b, int c) {
+        int[] val= {a, b, c};
+        Arrays.sort(val);
+        a= val[0]; b= val[1]; c= val[2];
+        
+        if(a+b<= c) return a+b;
+        else return c+ (a- (int)Math.ceil((c-(b-a))/2d));
+    }
+}
