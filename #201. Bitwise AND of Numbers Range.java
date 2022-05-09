a31ad1f43221febd562f9c09e9751926e8ed60b2
Adarsh Goswami <63430847+Adarsh-Goswamii@users.noreply.github.com>
2022-05-09 05:35:17 +0530

    Create #201. Bitwise AND of Numbers Range.java

---
+++ #201. Bitwise AND of Numbers Range.java
@@ -0,0 +1,14 @@
+
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        int ans = 0;
+        for(int i=0;i<31;i++) {
+            int val = 1<<i;
+            if((left&val)!= 0 && (right&val)!= 0 && right-left< val) {
+                ans|= val;
+            }
+        }
+        
+        return ans;
+    }
+}
