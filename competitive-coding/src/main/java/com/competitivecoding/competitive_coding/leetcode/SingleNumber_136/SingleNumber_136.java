38d8b08aca4279ebf4907c21cd0293199aca3a14
QuantumShivoham <prashant.a.srivastav007@gmail.com>
2024-09-22 05:26:15 +0530

    136 Single Number(Discard duplicate element using XOR)

---
+++ competitive-coding/src/main/java/com/competitivecoding/competitive_coding/leetcode/SingleNumber_136/SingleNumber_136.java
@@ -0,0 +1,20 @@
+package com.competitivecoding.competitive_coding.leetcode.SingleNumber_136;
+
+import com.competitivecoding.competitive_coding.base.ProblemSolver;
+
+public class SingleNUmber_136 extends ProblemSolver {
+@Override
+    public void solve() {
+        //solve here: Create Solution Object for leet Code else others and run its respective function.
+//        Solution solution = new Solution();
+//        Use io Ex: io.println();
+
+    }
+
+public static void main(String[] args) throws Exception {
+        new SingleNUmber_136().run();
+    }
+}
+class Solution{
+    //for LeetCode
+}
