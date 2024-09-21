c1803d84be7bee464a4b969ea1f888c11065ce1b
QuantumShivoham <prashant.a.srivastav007@gmail.com>
2024-09-21 07:00:48 +0530

    7.Reverse Integer LeetCode

---
+++ competitive-coding/src/main/java/com/competitivecoding/competitive_coding/leetcode/ReverseInteger_7.java
@@ -0,0 +1,42 @@
+package com.competitivecoding.competitive_coding.leetcode;
+
+import com.competitivecoding.competitive_coding.base.ProblemSolver;
+
+public class ReverseInteger_7 extends ProblemSolver {
+    @Override
+    public void solve() {
+/*
+        Solve here: Create Solution Object for leet Code else others and run its respective function.
+        Solution solution = new Solution();
+        Use IO operations like Ex: io.println();
+*/
+        Solution solution = new Solution();
+        var input = io.nextInt();
+        io.print(solution.reverse(input));
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        new ReverseInteger_7().run();
+    }
+}
+/*
+ * For LeetCode
+ * */
+
+class Solution {
+    public int reverse(int x) {
+        var maxState = (Integer.MAX_VALUE / 10);
+        var minState = (Integer.MIN_VALUE / 10);
+        var solution = 0;
+        while (x != 0) {
+            var d = x % 10;
+            if (solution > maxState || solution < minState) {
+                return 0;
+            }
+            solution = solution * 10 + d;
+            x /= 10;
+        }
+        return solution;
+    }
+}
