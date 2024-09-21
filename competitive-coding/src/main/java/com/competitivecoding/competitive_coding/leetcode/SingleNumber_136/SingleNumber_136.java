9cd95008d7d1541590a2dffc0fd7319c8b1ce4db
QuantumShivoham <prashant.a.srivastav007@gmail.com>
2024-09-22 05:26:28 +0530

    136 Single Number(Discard duplicate element using XOR)

--- competitive-coding/src/main/java/com/competitivecoding/competitive_coding/leetcode/SingleNumber_136/SingleNumber_136.java
+++ competitive-coding/src/main/java/com/competitivecoding/competitive_coding/leetcode/SingleNumber_136/SingleNumber_136.java
@@ -2,19 +2,70 @@ package com.competitivecoding.competitive_coding.leetcode.SingleNumber_136;
 
 import com.competitivecoding.competitive_coding.base.ProblemSolver;
 
-public class SingleNUmber_136 extends ProblemSolver {
-@Override
+import java.util.Arrays;
+
+public class SingleNumber_136 extends ProblemSolver {
+    @Override
     public void solve() {
         //solve here: Create Solution Object for leet Code else others and run its respective function.
 //        Solution solution = new Solution();
 //        Use io Ex: io.println();
+        Solution solution = new Solution();
+        var input = io.nextLine();
+        var nums = Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt).toArray();
+        io.print(solution.singleNumber(nums));
+
 
     }
 
-public static void main(String[] args) throws Exception {
-        new SingleNUmber_136().run();
+    public static void main(String[] args) throws Exception {
+        new SingleNumber_136().run();
     }
 }
-class Solution{
-    //for LeetCode
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ans = nums[i] ^ ans;
+        }
+        return ans;
+    }
 }
+
+/*
+ This below is a little slower than above due to use of ans variable in above
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            nums[i + 1] = nums[i] ^ nums[i + 1];
+        }
+        return nums[nums.length - 1];
+    }
+}
+*/
+/*
+This is O(n2) solution which is slower than above two but done for i, j will run from i+1, thus slicing array by 1 size
+Commented above as this property be used in optimization(not here).
+class Solution {
+    public int singleNumber(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            var iElement = nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                if (iElement == nums[j]) {
+                    nums[i] = -1;
+                    nums[j] = -1;
+                    break;
+              }
+            }
+        }
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != -1){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
+*/
